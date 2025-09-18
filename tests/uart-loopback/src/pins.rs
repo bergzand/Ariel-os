e4c7d140741658ba661c1e05659bb9caf61f0791
Koen Zandberg <koen@bergzand.net>
2025-09-18 14:42:13 +0200

    test(uart-loopback): add esp to test
    Signed-off-by: Koen Zandberg <koen@bergzand.net>

--- tests/uart-loopback/src/pins.rs
+++ tests/uart-loopback/src/pins.rs
@@ -1,1 +1,9 @@
 use ariel_os::hal::{peripherals, uart};
+
+#[cfg(context = "esp")]
+pub type TestUart<'a> = uart::UART0<'a>;
+#[cfg(context = "esp")]
+ariel_os::hal::define_peripherals!(Peripherals {
+    uart_tx: GPIO16,
+    uart_rx: GPIO17,
+});
